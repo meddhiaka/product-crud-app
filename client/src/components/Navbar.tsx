9f4ceca5ab5e6a9f6556fe4f177677a30a35ee7c
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2023-07-15 08:38:06 +0100

    some design fixed

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="max-w-5xl px-5 md:px-20 mx-auto flex flex-row bg-black justify-between">
                 <div className="text-white mx-1 md:mx-4 py-4 tracking-wider font-extrabold text-lg md:text-3xl"><Link to="/">All Products</Link></div>
-                <form className="flex w-auto items-center space-x-2 rounded-md p-1 m-3 bg-white focus:border-2 focus:border-red-950 ">
+                <form className="flex items-center space-x-2 rounded-md p-1 m-3 bg-white focus:border-2 focus:border-red-950 ">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 flex-none text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
