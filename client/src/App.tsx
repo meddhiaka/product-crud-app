5f2fbb4a5aef439288c540da6daf5e207b9afb3e
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2023-07-15 07:15:13 +0100

    React router added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
-
 import './App.css'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar/>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom"
 import './index.css'
+import Home from './pages/Home.tsx'
+import Product from './pages/Product.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='/' index={true} element={<Home />} />
+      <Route path='/product/:productName' element={<Product />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
