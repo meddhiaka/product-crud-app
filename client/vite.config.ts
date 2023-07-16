0f2092a11fed6c86972e4b7e6fb5b04ade21bdc9
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2023-07-16 01:48:36 +0100

    new origin added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://product-crud-app-axbw.onrender.com/'
+      '/api': 'https://server-product-crud.onrender.com/'
     }
   },
   plugins: [react()],
