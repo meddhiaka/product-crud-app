b5e7f9772448c38fd6c04d79434f44ea6d989706
Med Dhia Kassab <kassab@fsb.u-carthage.tn>
2023-07-16 02:34:40 +0100

    demo gif uploaded

--- client/src/components/SearchResultsComp.tsx
+++ client/src/components/SearchResultsComp.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 export default function SearchResultsComp({ resultsSearch }) {
+    console.log(resultsSearch)
     return (
         <div className="max-w-5xl px-5 mx-auto w-full bg-white flex flex-col  mt-[1rem] max-h-80 overflow-y-scroll">
             {
